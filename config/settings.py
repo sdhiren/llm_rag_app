5c479c0999df4bdd7821aa98b34f3adc49de6bd4
Dhiren Singh <singh.dhirendra07@gmail.com>
2025-09-12 00:18:40 +0530

    rag: minor refactoring

--- config/settings.py
+++ config/settings.py
@@ -3,10 +3,14 @@ import os
 import openai
 from dotenv import load_dotenv
 
-API_KEY_NAME = "OPENAI_API_KEY"
 load_dotenv()
+
+API_KEY_NAME = "OPENAI_API_KEY"
+EMBEDDING_MODEL_NAME = "text-embedding-3-small"
+
 openai.api_key = os.getenv(API_KEY_NAME)
-embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+embedding_model = OpenAIEmbeddings(model=EMBEDDING_MODEL_NAME)
+
 MODEL = "gpt-4o-mini"
 SYSTEM_MESSAGE = f"""
 You are a helpful assistant that answers user questions politely.
@@ -16,4 +20,3 @@ Answer clearly and concisely. If unsure, say so politely.
 
 DOC_INDEX_PATH = "../data/faiss_index/faiss_confluence_index"
 CODEBASE_INDEX_PATH = "../data/faiss_index/faiss_codebase_index"
-EMBEDDING_MODEL_NAME = "text-embedding-3-small"
